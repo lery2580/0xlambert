776c550a119ab3a5127d1b2587af626547ec8dcb
lery2580 <lery2580@gmail.com>
2021-09-23 22:32:53 +0800

    Update headerWidget.tsx

--- src/components/header/headerWidget.tsx
+++ src/components/header/headerWidget.tsx
@@ -7,7 +7,7 @@ function HeaderWidget() {
     <div className="header">
       <div className="headerWidget">
         <h1>
-          <a href={config.preLink} title="Soully">
+          <a href={config.preLink} title="SoullyWorld">
 
             <img src={require("./img/logo.png")} alt="logo" />
           </a>
